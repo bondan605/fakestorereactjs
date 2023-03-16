5d95f653c94f1353be5732f3bd5b8c90be3dc4b2
Ahmad Bondan Gunawan <73745874+bondan605@users.noreply.github.com>
2023-03-16 10:13:21 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+
+const App = () => {
+  return (
+    <div className="overflow-hidden">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
